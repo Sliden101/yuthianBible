3198b49c40895bc7373ae115a6b3590811de9a99
Sliden101 <chessplayz11@gmail.com>
2022-11-14 09:38:56 +0700

    hardcode add perms

--- commands/add.js
+++ commands/add.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 let {total} = require('../list.js');
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('add')
@@ -8,6 +9,7 @@ module.exports = {
             option.setName('quote')
                 .setDescription('The quote to add')),
 	async execute(interaction) {
+		if (!['820326898499977216', '481674569954885633', '424824780395380736', '487506921038020610', '489426266257358863'].includes(interaction.author.id)) return;
         let toAdd = interaction.options.getString('quote');
         total.push(toAdd);
 		await interaction.reply(`Added ${toAdd} to the list`).catch(err => {});
